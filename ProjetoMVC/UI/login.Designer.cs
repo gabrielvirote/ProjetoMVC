eccec370f321773ffd75733f90f7613a9978a1d3
gabriel virote <11900024@aluno.cotemig.com.br>
2020-05-27 19:18:31 -0300

    atualizaçao do projeto

--- ProjetoMVC/UI/login.Designer.cs
+++ ProjetoMVC/UI/login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetoMVC
 {
-    partial class Form1
+    partial class login
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,14 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "login";
+            this.Text = "login";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
